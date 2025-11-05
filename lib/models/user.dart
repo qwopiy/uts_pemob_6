553eab44d5aefed2202a135572d99099ee4a7eee
Bintang Khalik <bintangkhaliqq@gmail.com>
2025-11-05 15:29:00 +0700

    revert(database): remove sqflite dependencies, change to local dummy data

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,5 @@
 class User {
-  final int id;
+  final int id = 0;
   final String name;
   final int correct;
   final int wrong;
@@ -7,22 +7,10 @@ class User {
   final int totalCoinsUsed;
 
   const User({
-    required this.id,
     required this.name,
     required this.correct,
     required this.wrong,
     required this.coins,
     required this.totalCoinsUsed
   });
-
-  factory User.fromMap(Map<String, dynamic> map) {
-    return User(
-      id: map['id'],
-      name: map['name'],
-      correct: map['correct'],
-      wrong: map['wrong'],
-      coins: map['coins'],
-      totalCoinsUsed: map['total_coins_used']
-    );
-  }
 }
